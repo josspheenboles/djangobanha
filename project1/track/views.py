00b682aa5719854a6aec4f0887d2f7691147dae7
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-05 12:33:33 +0200

    template inheritance

--- project1/track/views.py
+++ project1/track/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def getalltracks(req):
     # return HttpResponse('<h1>getalltracks</h1>')
-    return  render(req,'tracks.html')
+    return  render(req,'page1.html')
 def addtrack(req):
     return HttpResponse('<h1>Add Track</h1>')
 def updatetrack(req,id):

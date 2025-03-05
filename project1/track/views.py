ce475badf9f5261240e09f0a8f41c25576316e5f
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-05 13:03:08 +0200

    Day1

--- project1/track/views.py
+++ project1/track/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def getalltracks(req):
+    context={}
+    context['tracks']=[
+        [1,'iot'],
+        [2, 'os'],
+        [3, 'python'],
+    ]
     # return HttpResponse('<h1>getalltracks</h1>')
-    return  render(req,'page1.html')
+    return  render(req,'track/all.html',context)
 def addtrack(req):
     return HttpResponse('<h1>Add Track</h1>')
 def updatetrack(req,id):

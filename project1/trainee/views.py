8cdf4b40dc14fdbbaffa5ae55d1639f4b6842323
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-09 14:36:02 +0200

    model add

--- project1/trainee/views.py
+++ project1/trainee/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def alltrainees(req):
     return render(req,'trainee/all.html')
 def add(req):
+    if(req.method=='POST'):
+        trname=req.POST['trname']
+        tremail=req.POST['tremail']
+        #upload image
+        trimg=req.FILES.get('trimg')
+        obj=Trainee()
+        obj.name=trname
+        obj.email=tremail
+        obj.image=req.POST['trimg']
+        obj.save()
     return render(req,'trainee/add.html')
 def update(req,id):
     return render(req,'trainee/update.html')

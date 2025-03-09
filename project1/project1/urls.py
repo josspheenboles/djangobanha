e2d9c9c1eba61554c4b76c306047a4cfecb07697
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-09 14:10:04 +0200

    upload media

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     #ref to urls app track
@@ -25,6 +26,4 @@ urlpatterns = [
     path('Trainee/',include('trainee.urls')),
     #ref to urls app myaccount
     path('',include('myaccount.urls')),
-
-
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

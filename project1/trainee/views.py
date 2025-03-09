f7165d03235b313489f1d3a30d09c557eace396c
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-09 14:52:13 +0200

    model add

--- project1/trainee/views.py
+++ project1/trainee/views.py
@@ -5,15 +5,17 @@ def alltrainees(req):
     return render(req,'trainee/all.html')
 def add(req):
     if(req.method=='POST'):
-        trname=req.POST['trname']
-        tremail=req.POST['tremail']
-        #upload image
-        trimg=req.FILES['trimg']
-        obj=Trainee()
-        obj.name=trname
-        obj.email=tremail
-        obj.image=trimg
-        obj.save()
+        # trname=req.POST['trname']
+        # tremail=req.POST['tremail']
+        # #upload image
+        # trimg=req.FILES['trimg']
+        # obj=Trainee()
+        # obj.name=trname
+        # obj.email=tremail
+        # obj.image=trimg
+        # obj.save()
+        Trainee.objects.create(name=req.POST['trname'],email=req.POST['tremail'],
+                               image=req.FILES['trimg'])
     return render(req,'trainee/add.html')
 def update(req,id):
     return render(req,'trainee/update.html')

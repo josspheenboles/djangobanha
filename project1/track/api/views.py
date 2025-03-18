b18fd3a118ba9426e54cbf5b1cc7b0ee2650da57
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-18 10:22:51 +0200

    track all

--- project1/track/api/views.py
+++ project1/track/api/views.py
@@ -1,10 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from ..models import Track
 
 @api_view(['GET'])
 def helloworld(request):
     return Response(
         data={'msg':'hello python tracks'},
         status=status.HTTP_200_OK
+    )
+
+@api_view(['GET'])
+def getall(reaquest):
+    tracks=Track.getalltracks()
+    return Response(
+        data={'tracks':tracks}
     )
--- project1/track/urls.py
+++ project1/track/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
-from .api.views import *
+from .api import views
 urlpatterns=[
     path('', getalltracks,name='tracks'),
     path('New/', addtrack,name='addtrack'),
     path('Update/<int:id>/', updatetrack),
     #endpoints
-    path('hw/',helloworld)
+    path('hw/',views.helloworld),
+    path('all', views.getall),
 ]

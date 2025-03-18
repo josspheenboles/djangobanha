a3e661a0436da9e0f8316b38295802e4f80766ac
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-18 10:08:33 +0200

    clean api

--- api/testapp/views.py
+++ api/testapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+# Restfullview restrication method
+@api_view(['POST'])
 def helloworld(requset):
-    pass
+    print(requset.data)
+    if('name' in requset.data):
+        return Response(
+            data=requset.data,
+            status=status.HTTP_200_OK
+
+        )
+    else:
+        return Response(
+            data={'error':'name must b send'},
+            status=status.HTTP_400_BAD_REQUEST
+        )

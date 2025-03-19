77542762d689caa344c46bdf281009c1a7fcff50
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-19 13:56:05 +0200

    email configuration

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -142,3 +142,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+###Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'xxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxx'
+AUTH_USER_MODEL = 'myaccount.CustomUser'

17daedc9c78ed5fe41201b347fbfe0fff89cf3fe
josspheenboles <eng.jospheenboles@gmail.com>
2025-03-19 16:05:07 +0200

    consume rest_api

--- consumeapi.py
+++ consumeapi.py
@@ -2,13 +2,21 @@
 import requests
 #endpoint
 endpoint='http://localhost:8000/Track/all/'
+
+#add php track
+d={'name':'php'}
+res=requests.post(endpoint,data=d)
+print(res.status_code)
+
 #method get
+
 
 respon=requests.get(endpoint)
 print(respon.status_code,type(respon.status_code))
 if(respon.status_code==200):
     for track in respon.json()['tracks']:
-        print(f'ID:{{track["id"}},Name:{{track["name"]}}')
+        print(track)
+        # print(f'ID:{{track["id"}},Name:{{track["name"]}}')
 else:
     print('invalid endijtn')
 
